572bee013a15430f9dc3e61b93af487f07151280
Rich <richard@9by5.co.uk>
2020-01-25 19:46:43 +0100

    Update 600-config.sh

--- scripts/600-config.sh
+++ scripts/600-config.sh
@@ -5,7 +5,7 @@ echo "Installing CommandBox"
 
 curl -fsSl https://downloads.ortussolutions.com/debs/gpg | apt-key add -
 echo "deb https://downloads.ortussolutions.com/debs/noarch /" | tee -a /etc/apt/sources.list.d/commandbox.list
-apt-get update && apt-get install commandbox
+yum update && yum install commandbox
 
 box install commandbox-cfconfig
 
